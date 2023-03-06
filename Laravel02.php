e6ff545ff8a6e5629ceb43e0e46260bba8bbaf2e
m-shiochi <mshiochi66@gmail.com>
2023-03-06 05:15:12 +0000

    work2

--- Laravel02.php
+++ Laravel02.php
@@ -3,21 +3,29 @@
 $a = 3;
 $b = 7;
 echo $a+$b;
+echo "\n";
+echo "\n";
 
 //work2
-$array_month = ["1月", "2月", "３月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
-echo $array_month[9];
+$array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
+echo $array_month[7];
+echo "\n";
+echo "\n";
 
 //work3
 $hello = "Hellow";
 $name = "Masayuki";
 $world = "'s World!";
 echo $hello.$name.$world;
+echo "\n";
+echo "\n";
 
 //work4
 $tech_boost = 'tech';
 $tech_boost .= ' boost';
 echo $tech_boost;
+echo "\n";
+echo "\n";
 
 //work5
 $calendar_2018 = [
@@ -37,3 +45,5 @@ $calendar_2018 = [
 
 // 12月を表示する
 echo $calendar_2018["December"];
+echo "\n";
+echo "\n";
